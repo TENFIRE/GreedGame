7f1c5aece1141c530b7ff66ba4cc2b461222752c
Erik Nilsson <nil_erik@hotmail.com>
2013-12-04 00:25:24 +0100

    Added CheckCallback

--- src/GUI/MyPanel.java
+++ src/GUI/MyPanel.java
@@ -2,6 +2,7 @@ package GUI;
 
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public abstract class MyPanel extends JPanel implements ActionListener
@@ -11,5 +12,15 @@ public abstract class MyPanel extends JPanel implements ActionListener
 	
 	public abstract void UpdateData();
 	public void SetCallback(GUI_Callback callback) { this.callback = callback; }
+	
+	protected boolean CheckCallback() 
+	{ 
+		if (callback == null)
+		{
+			JOptionPane.showMessageDialog(null, "Can't contact game engine.");
+			return false;
+		}
+		return true;
+	}
 	
 }
